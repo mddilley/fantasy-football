862b3ee235f81b911ab47f714e244ad689874017
Mike Dilley <mike.dilley@gmail.com>
2018-11-12 16:58:26 +0000

    first commit

---
+++ lib/fantasy/football.rb
@@ -0,0 +1,8 @@
+require "fantasy/football/version"
+
+module Fantasy
+  module Football
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/fantasy/football/version.rb
@@ -0,0 +1,5 @@
+module Fantasy
+  module Football
+    VERSION = "0.1.0"
+  end
+end
