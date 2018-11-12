f67d15986618be60574a028f6ef7971540f06403
Mike Dilley <mike.dilley@gmail.com>
2018-11-12 17:38:42 +0000

    Added objects

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,3 +1,23 @@
 class CLI
 
+  def welcome
+    puts "Welcome to the NFL Fantasy Football Rankings and Players!"
+  end
+
+  def show_rankings
+    # asks for position (QB, WR, RB, TE, or K) and lists top 20 ranked by ESPN
+  end
+
+  def show_player
+    # prompts for player details
+    # input is rank, output is player details
+  end
+
+  def run
+    welcome
+    show_rankings
+    show_player
+    
+  end
+
 end
---
+++ lib/scraper.rb
@@ -0,0 +1,13 @@
+class Scraper
+
+  def scrape_player(player_url)
+    #scrapes ESPN.com for players listed in rankings
+    #variable passed in is player url
+  end
+
+  def scrape_rankings(ranking_url)
+    # scrapes ESPN.com for top 20 players per position
+    # variable pass in is ranking url for position
+  end
+
+end
