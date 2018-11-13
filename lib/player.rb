b8bb6f3278c963ade379095a40acdcdcb60acb77
Mike Dilley <mike.dilley@gmail.com>
2018-11-13 19:35:55 +0000

    Added create_from_hash method to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -2,9 +2,20 @@
 
 class Player
 
-  def initialize(player_hash)
+  def initialize
     # player_hash passed in contains player name and attributes
     # use metaprogramming to instantiate new classes and create attributes
+  end
+
+  def create_from_hash(player_hash)
+    player = Player.new
+    player_hash.each do |k,v|
+      player.send("#{k}=", v)
+    end
+  end
+
+  def create_from_nested_hashes(nested_hash)
+
   end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,14 +7,23 @@ class Scraper
     # variable passed in is player_url
     # output is hash of player attributes
     doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/players/patrick-mahomes.php"))
-    # player name => doc.css('.player-16413').text
-    # player position => doc.css('div .pull-left h5').text.strip.split(" - ")[0]
-    # player projection => doc.css('.clearfix.detail span.pull-right')[2].text.split[0]
-    # player team => doc.css('div .pull-left h5').text.strip.split(" - ")[1]
-    # player height => doc.css('span.bio-detail')[0].text[8,5]
-    # player weight => doc.css('span.bio-detail')[1].text[8,3]
-    # player age => doc.css('span.bio-detail')[2].text[5,2]
-    # player college => doc.css('span.bio-detail')[3].text.split(": ")[1]
+    # player :name => doc.css('.player-16413').text
+    # player :position => doc.css('div .pull-left h5').text.strip.split(" - ")[0]
+    # player :projection => doc.css('.clearfix.detail span.pull-right')[2].text.split[0]
+    # player :team => doc.css('div .pull-left h5').text.strip.split(" - ")[1]
+    # player :height => doc.css('span.bio-detail')[0].text[8,5]
+    # player :weight => doc.css('span.bio-detail')[1].text[8,3]
+    # player :age => doc.css('span.bio-detail')[2].text[5,2]
+    # player :college => doc.css('span.bio-detail')[3].text.split(": ")[1]
+    hash = {:name => doc.css('h1').text,
+            :position => doc.css('div .pull-left h5').text.strip.split(" - ")[0],
+            :projection => doc.css('.clearfix.detail span.pull-right')[2].text.split[0],
+            :team => doc.css('div .pull-left h5').text.strip.split(" - ")[1],
+            :height => doc.css('span.bio-detail')[0].text[8,5],
+            :weight => doc.css('span.bio-detail')[1].text[8,3],
+            :age => doc.css('span.bio-detail')[2].text[5,2],
+            :college => doc.css('span.bio-detail')[3].text.split(": ")[1]
+            }
   end
 
   def scrape_rankings(position)
