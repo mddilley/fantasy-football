e1d5f9fd7b99dfedf98ab52d74e8baedfc69356b
Mike Dilley <mike.dilley@gmail.com>
2018-11-16 22:03:24 +0000

    Removed custom class constructor from Player class

--- lib/player.rb
+++ lib/player.rb
@@ -10,21 +10,11 @@ class Player
   @@all = []
 
   def initialize
-    # player_hash passed in contains player name and attributes
-    # use metaprogramming to write attributes
-    # player_hash.each {|k,v| self.send("#{k}=", v)}
     save
   end
 
   def self.all # Class variable reader
     @@all
-  end
-
-  def self.create_from_nested_hashes(nested_hash)
-    # Custom class constructor, accepts player objects nested in an array
-    nested_hash.each do |p|
-      Player.new(p)
-    end
   end
 
 end
